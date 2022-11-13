424557279ae296559f72bfb288e7f4a315125dbd
Abilio Krismanuel <abilio.krismanuel@ti.ukdw.ac.id>
2022-11-13 23:11:37 +0700

    done chart js

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <PlanetChart/>
+  </div>
+</template>
+
+<script>
+import PlanetChart from './components/PlanetChart.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PlanetChart
+  }
+}
+</script>
+
+
